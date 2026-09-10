2cb478c88e32e28af7ce22b015f856008c26eae5
Jaytwin1 <Jaytwins1w@yahoo.com>
2026-09-10 15:23:38 -0400

    Trying to make gradle fail

--- src/NoBueno.java
+++ src/NoBueno.java
@@ -1,5 +1,5 @@
 public class NoBueno {
     public static void main(String[] args){
-    System.out.print("Listen to Celldweller!");
+    System.out.print("Listen to Celldweller!                         ");
 }
 }
